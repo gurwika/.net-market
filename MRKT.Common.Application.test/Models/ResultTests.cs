90c80ab7c1e34383b3fbee48233db3872e3b22dc
gurwika <gurwika@gmail.com>
2019-11-02 00:09:48 +0000

    Add Application tests

---
+++ MRKT.Common.Application.test/Models/ResultTests.cs
@@ -0,0 +1,33 @@
+﻿using MRKT.Common.Application.Common.Models;
+using System.Collections.Generic;
+using Xunit;
+
+namespace MRKT.Common.Application.test.Models
+{
+    public class ResultTests
+    {
+        [Fact]
+        public void ShouldMatchSuccessStatus()
+        {
+            var successResult = Result.Success();
+
+            Assert.True(successResult.Succeeded);
+            Assert.Empty(successResult.Errors);
+        }
+
+        [Fact]
+        public void ShouldMatchFailureStatus()
+        {
+            var errorList = new List<string>()
+            {
+                "validation error"
+            };
+
+            var successResult = Result.Failure(errorList);
+
+            Assert.False(successResult.Succeeded);
+            Assert.NotEmpty(successResult.Errors);
+            Assert.Single(successResult.Errors);
+        }
+    }
+}
